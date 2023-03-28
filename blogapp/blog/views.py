14ec5cd43fbe13253efae0510663c73d8ff4b4f4
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2023-03-28 16:19:32 +0300

    created project

---
+++ blogapp/blog/views.py
@@ -0,0 +1,17 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+# http requestlerimiz.
+def index(request):
+    return render(request, "blog/index.html")
+
+
+def blogs(request):
+    return render(request, "blog/blogs.html")
+
+
+def blog_details(request, id):
+    return render(request, "blog/blog-details.html", {
+        "id": id
+    })
