b01978f5f2a7052375be26b4f45eca1e7989d37a
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2023-03-28 17:05:49 +0300

    blog templates created

--- blogapp/blogapp/settings.py
+++ blogapp/blogapp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent  # ana klasörün yolu yani blogapp
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'blogapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
